6dc0dbd47ec25b2293e328efda07503018fe4c20
wencai.xu <wencai.xu@ipinyou.com>
2020-06-16 23:45:45 +0800

    feat:utils

--- Utils.sh
+++ Utils.sh
@@ -1,18 +1,18 @@
-function pow(){
-  echo $[ $1 * $1 ]
-}
+#************************************************************************
+# swap two element function
+# @param element1
+# @param element2
+# @return index
+#************************************************************************
 
-dir=/data/v6-logs/$1/$2
-sum=0;
-for log in $dir/*; do
-    records=$(awk -f log.awk -v var='1.6 1.12' 201910231429.00a.impression.log | sort | uniq -c | sort -r | awk '{print $2,$1}')
-    val=$(echo $records | sed 's/[[:space:]]//g')
-    if test $val
-    then
-      echo $val;
-      # sum=$[ $sum + $val ];
-    else
-      echo $log
-    fi
-done
-echo $sum;
+swap(){
+  # local t1=$1; //local can only use in inner function
+  local t1=$1;
+  local t2=$2;
+  t1=$[ $t1 ^ $t2 ];
+  t2=$[ $t1 ^ $t2 ];
+  t1=$[ $t1 ^ $t2 ];
+  echo $t1,$t2;
+}
+swap 1 2
+sleep 10s
