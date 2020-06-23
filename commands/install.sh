15ece5bc66b7aa3b6f3df8fc177b12c16e8bafb1
wencaixu <JedisXu@163.com>
2020-06-23 10:47:31 +0800

    feat:add command

---
+++ commands/install.sh
@@ -0,0 +1,20 @@
+main(){
+  while [ -n "$1" ]
+  do
+      case "$1" in
+      -a)
+        echo "Param -a"
+        ;;
+      -b)
+        echo "Param -b"
+        ;;
+      *)
+        echo "None"
+        ;;
+      esac
+      shift
+  done
+  sleep 10s
+}
+
+main -a -b
